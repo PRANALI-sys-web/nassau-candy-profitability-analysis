325490f4c2fca5e16306e718a9c8ab86e3b86d67
PRANALI-sys-web <pranaliwakchaure5635@gmail.com>
2026-07-21 19:40:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-st.set_page_config(layout="wide")
+st.set_page_config(
+    page_title="Sales Performance Dashboard",
+    page_icon="📊",
+    layout="wide"
+)
 st.markdown("""
 <style>
 
